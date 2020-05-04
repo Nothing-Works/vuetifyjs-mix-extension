587080943c4cebb56c53bc02812f39acf650b479
AndySong <andylauszp@gmail.com>
2020-05-05 11:15:38 +1200

    fix tests

--- tests/resolve-parameter.test.js
+++ tests/resolve-parameter.test.js
@@ -12,33 +12,33 @@ describe('type check', () => {
 describe('has object option check', () => {
     test('should return same object for optionObject', () => {
         const pa = rp([{}])
-        expect(pa.optionObject).toEqual({})
+        expect(pa.option).toEqual({})
     })
 })
 
 describe('has file string check', () => {
     test('should return path for fileString', () => {
         const pa = rp(['src/sass/variable.scss'])
-        expect(pa.fileString).toBe('src/sass/variable.scss')
+        expect(pa.file).toBe('src/sass/variable.scss')
     })
 })
 
 describe('pass two params check order one', () => {
     const pa = rp(['src/sass/variable.scss', { foo: 'bar' }])
     test('should return same file', () => {
-        expect(pa.fileString).toBe('src/sass/variable.scss')
+        expect(pa.file).toBe('src/sass/variable.scss')
     })
     test('should return same object', () => {
-        expect(pa.optionObject).toEqual({ foo: 'bar' })
+        expect(pa.option).toEqual({ foo: 'bar' })
     })
 })
 
 describe('pass two params check order two', () => {
     const pa = rp([{ foo: 'bar' }, 'src/sass/variable.scss'])
     test('should return same file', () => {
-        expect(pa.fileString).toBe('src/sass/variable.scss')
+        expect(pa.file).toBe('src/sass/variable.scss')
     })
     test('should return same object', () => {
-        expect(pa.optionObject).toEqual({ foo: 'bar' })
+        expect(pa.option).toEqual({ foo: 'bar' })
     })
 })
